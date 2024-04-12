1a1d99e2ce7daa862fdf43289e6e49a2d48baa0b
aymane-hamdi <aymanhamdi037@gmail.com>
2024-04-12 12:58:25 -0400

    ok

--- fdf.c
+++ fdf.c
@@ -27,13 +27,41 @@ void printf_matrix(char **argv,char ***matrix)
         i++;
     }
 }
-int main (int argc,char **argv)
+
+int close_window(void *param)
 {
-    // char ***matrix;
+    ft_putstr_fd("windows close\n",1);
+    exit(0); // Quitte le programme proprement
+}
+
+int main(int argc, char **argv)
+{
     void *mlx_ptr;
     void *win_ptr;
+    void *img_ptr;
+    int width = 200, height = 200;
 
     mlx_ptr = mlx_init();
-    win_ptr = mlx_new_window(mlx_ptr, 500, 5000, "FDF project");
+    if (mlx_ptr == NULL)
+    {
+        printf("Failed to initialize mlx.\n");
+        return 1;
+    }
+    win_ptr = mlx_new_window(mlx_ptr, width, height, "FDF project");
+    if (win_ptr == NULL)
+    {
+        printf("Failed to create a new window.\n");
+        return 1;
+    }
+
+    img_ptr = mlx_xpm_file_to_image(mlx_ptr, argv[1], &width, &height);
+    if (img_ptr == NULL)
+    {
+        printf("Failed to load the image.\n");
+        return 1;
+    }
+    mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 40, 40);
+    mlx_hook(win_ptr, 17, 0, close_window, NULL); 
     mlx_loop(mlx_ptr);
+    return(0);
 }
